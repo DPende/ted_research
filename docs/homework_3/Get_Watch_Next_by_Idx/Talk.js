4d939471e9c7c3cff181283f454ad9d76ff4a05b
Andrea Rota <andrea.rota.98@gmail.com>
2023-05-11 16:32:06 +0200

    Get_Watch_Next_by_Idx
    Lambda function "Get_Watch_Next_by_Idx"

---
+++ docs/homework_3/Get_Watch_Next_by_Idx/Talk.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const talk_schema = new mongoose.Schema({
+    _id: String,
+    main_speaker: String,
+    title: String,
+    url: String,
+    posted: String,
+    details: String,
+    main_author: String,
+    num_views: Number,
+    watch_nexts: Array
+}, { collection: 'tedx_data' });
+
+module.exports = mongoose.model('talk', talk_schema);
+
